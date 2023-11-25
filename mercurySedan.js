afc93403b44c8e82f13b467f8e6d2cf607cb7a8f
Edward E Greenwood <edwardgreenwood38@gmail.com>
2023-11-25 17:06:38 -0500

    completed

--- mercurySedan.js
+++ mercurySedan.js
@@ -1,9 +1,9 @@
-//this includes the vehicle class as a module
-const VehicleModule = require("./vehicleBaseClass")
+// //this includes the vehicle class as a module
+// const VehicleModule = require("./vehicleBaseClass.js")
 
-//this shows how to call from this module...
-let v = new VehicleModule.Vehicle("Mercury", "Sedan", "1965", "color", "mileage");
-console.log(v.make)
+// //this shows how to call from this module...
+// let v = new VehicleModule.Vehicle("Mercury", "Sedan", "1965", "color", "mileage");
+// console.log(v.make)
 
 
 //After you write the derived Car class, you should test it out.
@@ -13,14 +13,80 @@ console.log(v.make)
 
 //TO DO: Code the Car subclass here or in index.js file, i.e. class Car extends Vehicle ...
 
+// You have been assigned the task of developing a new class for the new A28 Mercury sedan. The class will be written in JavaScript and will contain vehicle properties and methods that have previously been defined in the base class named vehicle.js. The new class is required to have the following properties and methods added.
+
+// A28 Mercury Sedan
+class Car extends Vehicle {
+    constructor(make, model, year, color, mileage) {
+        super(make, model, year, color, mileage);
+        this.maximumPassengers = 5;
+        this.passengers = 0;
+        this.numberOfWheels = 4;
+        this.maximumSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false;
+    }
+
+    loadPassenger(num) {
+        if (this.passengers < this.maximumPassengers){
+            if(this.passengers + num < this.maximumPassengers) {
+                console.log(this.year + " " + this.nake + " sedan has " + this.passengers + " loaded now");
+                return this.passengers += num;
+            }
+            else {
+                let total = this.passengers + num
+                console.log(this.year + " " + this.make + " sedan cannot hold " + total + " passengers.");
+            }
+             
+        }
+        else {
+            console.log(this.year + this.make + " can only have " + this.maximumPassengers + " passengers");
+        }
+    }
+
+    start() {
+        if(this.fuel > 0) {
+            console.log(this.make + " started");
+            return this.start = true;
+        }
+        else {
+            console.log(this.make + " is out of fuel");
+            return this.start = false;
+        }
+    }
+
+    checkService(due) {
+        if(this.mileage >= 30000){
+            console.log("Services are due");
+            return this.scheduleService = true;
+        }
+        else {
+            console.log("No services are due right now");
+            return this.scheduleService = false;
+        }
+    }
+}
 
 
+let myCar = new Car('mercury', 'rad_sedan', '2002', 'white', 50000)
+
+myCar.start()
+myCar.loadPassenger(5)
+myCar.stop()
+myCar.checkService()
+
+console.log(myCar)
 
 
+let myCar2 = new Car('chevy', 'sedan', '2019', 'silver', 4000)
 
 
+myCar2.start()
+myCar2.loadPassenger(6)
+myCar2.stop()
+myCar2.checkService()
 
-
+console.log(myCar2)
 
 
 
